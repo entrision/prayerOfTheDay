5a112f1bdc5b75ec37b91b833b966d4731b82c46
Travis Wade <travis@Travis-DevMac.local>
2015-03-26 12:47:30 -0700

    - select prayer UI - autolayout

---
+++ PrayerOfTheDay/SelectPrayerViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SelectPrayerViewController.swift
+//  PrayerOfTheDay
+//
+//  Created by Travis Wade on 3/26/15.
+//  Copyright (c) 2015 OperationBlessing. All rights reserved.
+//
+
+import UIKit
+
+class SelectPrayerViewController: OperationBlessingBaseViewController {
+
+    @IBOutlet var todayImage: UIImageView!
+    @IBOutlet var day1Image: UIImageView!
+    @IBOutlet var day2Image: UIImageView!
+    @IBOutlet var day3Image: UIImageView!
+    @IBOutlet var day4Image: UIImageView!
+    @IBOutlet var day5Image: UIImageView!
+    @IBOutlet var day6Image: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PrayerOfTheDay/SinglePrayerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SinglePrayerViewController.swift
+//  PrayerOfTheDay
+//
+//  Created by Travis Wade on 3/26/15.
+//  Copyright (c) 2015 OperationBlessing. All rights reserved.
+//
+
+import UIKit
+
+class SinglePrayerViewController: OperationBlessingBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
